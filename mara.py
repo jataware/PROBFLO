d577800f6475cd2b0b65a1b91f4e303d0b2efeaf
David Samson <david.andrew.engineer@gmail.com>
2023-01-04 10:48:04 -0500

    moved custom enum class into utilities folder. updated gitignore with .env and .env.leave for autoenv files

--- mara.py
+++ mara.py
@@ -1,24 +1,8 @@
 from NeticaPy import Netica
-from enum import Enum, EnumMeta, _EnumDict
 import json
 import pandas as pd
 import sys
-
-#custom enum metaclass so that calling MyEnum.MY_VALUE returns b'MY_VALUE'
-#str method should return just the name of the enum value
-class MyEnumMeta(EnumMeta):
-    def __new__(metacls, cls, bases, oldclassdict):
-        newclassdict = _EnumDict()
-        newclassdict._cls_name = cls
-        for key, value in oldclassdict.items():
-            if value == ():
-                value = key.encode('utf-8')
-            newclassdict[key] = value
-        return super().__new__(metacls, cls, bases, newclassdict)
-class MyEnum(bytes, Enum, metaclass=MyEnumMeta):
-    """base class for enum where values are bytes of the enum name"""
-    def __str__(self):
-        return self.name
+from utilities import MyEnum
 
 
 class Val(MyEnum):
