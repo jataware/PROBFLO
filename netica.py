0518510bc3f5fae38dded9afd2576a0987650676
David Samson <david.andrew.engineer@gmail.com>
2023-03-07 13:36:10 -0500

    created a better api for accessing the netica graph

---
+++ netica.py
@@ -0,0 +1,126 @@
+
+from NeticaPy import Netica, NewNode as NeticaNode
+from typing import Generator
+import os
+
+import pdb
+
+#TODO: handling errors that the netica API returns. i.e. self.res
+
+N = Netica()
+class NeticaManager:
+    def __init__(self, password_varname="NETICA_PASSWORD"):
+        # get the password from the environment variable
+        password = os.environ.get(password_varname, default="")
+        
+        # create the netica environment
+        self.env = N.NewNeticaEnviron_ns(password.encode('utf-8'), None, b"")
+        self.mesg = bytearray()
+        self.res = N.InitNetica2_bn(self.env, self.mesg)
+
+    def new_graph(self, path:str) -> "NeticaGraph":
+        path = path.encode('utf-8')
+
+        #load the network
+        net = N.ReadNet_bn(N.NewFileStream_ns(path, self.env, b""), 0)
+        N.CompileNet_bn(net)
+
+        return NeticaGraph(net, self)
+
+class NeticaGraph:
+    def __init__(self, net, manager:NeticaManager):
+        self.net = net
+        self.manager = manager
+
+        self.node_names = {self.node_name(i): i for i in range(self.num_nodes())}
+
+    def num_nodes(self) -> int:
+        """get the number of nodes in a network"""
+        return N.LengthNodeList_bn(N.GetNetNodes_bn(self.net))
+
+    def net_itr(self) -> Generator[NeticaNode, None, None]:
+        """iterator over the nodes in a network"""
+        nodes = N.GetNetNodes_bn(self.net)
+        
+        for i in range(N.LengthNodeList_bn(nodes)):
+            yield N.NthNode_bn(N.GetNetNodes_bn(self.net), i)
+
+    def get_node_by_index(self, node_idx:int) -> NeticaNode:
+        """get a node by its index"""
+        return N.NthNode_bn(N.GetNetNodes_bn(self.net), node_idx)
+    
+    def get_node_by_name(self, node_name:str) -> NeticaNode:
+        #TODO: this is a pretty inefficient implementation...is there a netica API call for getting a node by name?
+        # all_node_names = [self.node_name(i) for i in range(self.num_nodes())]
+        # node_idx = all_node_names.index(node_name)
+        # return self.get_node_by_index(node_idx)
+        node_idx = self.node_names[node_name]
+        return self.get_node_by_index(node_idx)
+    
+    def get_node(self, node:int|str|NeticaNode) -> NeticaNode:
+        """get a node by either its index or name"""
+        if isinstance(node, str):
+            return self.get_node_by_name(node)
+        elif isinstance(node, int):
+            return self.get_node_by_index(node)
+        elif isinstance(node, NeticaNode):
+            return node
+        else:
+            raise TypeError(f"node must be either a string, int, or NeticaNode, not {type(node)}")
+
+    def get_node_state(self, node:int|str|NeticaNode, state:int|str) -> str:
+        """get the name of a state of a node"""
+        node = self.get_node(node)
+        if isinstance(state, int):
+            return self.get_node_state_name(node, state)
+        elif isinstance(state, str):
+            all_node_states = [self.get_node_state_name(node, i) for i in range(self.get_node_num_states(node))]
+            assert state in all_node_states, f"state {state} not in node {self.node_name(node)}'s states: {all_node_states}"
+            return state
+        else:
+            raise TypeError(f"state must be either a string or int, not {type(state)}")
+    
+    def node_name(self, node:int|str|NeticaNode) -> str:
+        #TODO: -> node comes from net_itr... maybe make this just take in the index of the node?
+        """get the name of a node"""
+        node = self.get_node(node)
+        return N.GetNodeName_bn(node).decode('utf-8')
+
+    def node_type(self, node:int|str|NeticaNode) -> int: #TODO: extract the string this refers to...
+        #TODO: -> node comes from net_itr... maybe make this just take in the index of the node?
+        """get the type of a node. node may be either the index, or the name of the node"""
+        node = self.get_node(node)
+        return N.GetNodeType_bn(node)
+
+    def node_kind(self, node:int|str|NeticaNode) -> int: #TODO: extract the string this refers to...
+        #TODO: -> node comes from net_itr... maybe make this just take in the index of the node?
+        """get the kind of a node"""
+        node = self.get_node(node)
+        return N.GetNodeKind_bn(node)
+
+    def get_node_num_states(self, node:int|str|NeticaNode) -> int:
+        #TODO: -> node comes from net_itr... maybe make this just take in the index of the node?
+        """get the number of states of a node cane take on, or 0 if it is a continuous node"""
+        node = self.get_node(node)
+        return N.GetNodeNumberStates_bn(node)
+
+    def get_node_state_name(self, node:int|str|NeticaNode, state_index) -> str:
+        #TODO: -> node comes from net_itr... maybe make this just take in the index of the node?
+        """get the name of a state of a node"""
+        node = self.get_node(node)
+        return N.GetNodeStateName_bn(node, state_index).decode('utf-8')
+    
+    def enter_finding(self, node:int|str|NeticaNode, state:int|str, verbose=False):
+        node = self.get_node(node)
+        node_name = self.node_name(node)
+        node_state = self.get_node_state(node, state)
+        N.EnterFinding(node_name.encode('utf-8'), node_state.encode('utf-8'), self.net)
+        if verbose:
+            print(f"setting {node_name} to {node_state}")
+
+    def get_node_belief(self, node:int|str|NeticaNode, state:int|str) -> float:
+        node = self.get_node(node)
+        node_name = self.node_name(node)
+        node_state = self.get_node_state(node, state)
+        belief = N.GetNodeBelief(node_name.encode('utf-8'), node_state.encode('utf-8'), self.net)
+        return belief
---
+++ test.py
@@ -0,0 +1,62 @@
+from netica import NeticaManager
+
+import pdb
+
+paths = [
+    'neta/limpopo.neta',
+    'next_stuff/PROBFLO/5_subbasin/2021-11-16 BN Limpopo CROC_Update CPT_EcoEnd.neta',
+    'next_stuff/PROBFLO/5_subbasin/ELEP-Y30C-SINGU_Update CPT_EcoE.neta',
+    'next_stuff/PROBFLO/5_subbasin/LIMP-A71L-MAPUN_Update CPT_EcoE.neta',
+    'next_stuff/PROBFLO/5_subbasin/LIMP-Y30D-PAFUR FIXED_Update CPT_EcoE.neta',
+    'next_stuff/PROBFLO/5_subbasin/LIMP-Y30F-CHOKW_Update CPT_EcoE.neta'
+]
+
+def main():
+    netica = NeticaManager()
+
+    for path in paths:
+        graph = netica.new_graph(path)
+        for node in graph.net_itr():
+            print(f'{graph.node_name(node)=}')
+            node_name = graph.node_name(node)
+            node_by_name = graph.get_node_by_name(node_name)
+            print(f'{node_by_name==node=}')
+            for state in range(graph.get_node_num_states(node)):
+                print(f'    {graph.get_node_state_name(node, state)} = {graph.get_node_belief(node, state)}')
+            print()
+        print('------------------------------------------------------------------')
+
+    # all_names = []
+    # for path in paths:
+    #     graph = netica.new_graph(path)
+    #     names = set()
+    #     for node_idx in range(graph.num_nodes()):
+    #         print(graph.node_name(node_idx))
+    #         names.add(graph.node_name(node_idx))
+    #     all_names.append(names)
+
+
+    #     print()
+
+    # #check if all the names are the same
+    # for i in range(len(all_names)):
+    #     for j in range(i+1, len(all_names)):
+    #         print(f'{i=}, {j=}')
+    #         print(f'{all_names[i] - all_names[j]=}')
+    #         print(f'{all_names[j] - all_names[i]=}')
+    #         print()
+
+    # graph = netica.new_graph("next_stuff/PROBFLO/5_subbasin/2021-11-16 BN Limpopo CROC_Update CPT_EcoEnd.neta")
+    # # print(f'{graph.num_nodes()=}')
+    # for node_idx in range(graph.num_nodes()): #graph.net_itr():
+    #     print(f'name: {graph.node_name(node_idx)}')
+    #     # print(f'type: {graph.node_type(node_idx)}')
+    #     # print(f'kind: {graph.node_kind(node_idx)}')
+
+    #     # states = [graph.get_node_state_name(node_idx, i) for i in range(graph.get_node_num_states(node_idx))]
+    #     # print(f'states: {states}')
+
+    #     # print()
+
+if __name__ == "__main__":
+    main()
