c06458785b2c6e831eb931f687c8b55e45031a70
David Samson <david.andrew.engineer@gmail.com>
2023-03-09 12:41:47 -0500

    updated limpopo.py to work with the new API/match approach from 5-subbasin version

--- limpopo.py
+++ limpopo.py
@@ -3,54 +3,36 @@ import json
 import pandas as pd
 import numpy as np
 
-column_mapper = {'SUB_FISH_END-mean': 'Maintaining fisheries for livelihoods (Mean)',
- 'SUB_VEG_END-mean': 'Maintain plants for livelihoods (Mean)',
- 'LIV_VEG_END-mean': 'Maintain plants for domestic livestock (Mean)',
- 'DOM_WAT_END-mean': 'Maintain water for domestic use (Mean)',
- 'FLO_ATT_END-mean': 'Flood attenuation services (Mean)',
- 'RIV_ASS_END-mean': 'River assimilation capacity (Mean)',
- 'WAT_DIS_END-mean': 'Maintain water borne diseases (Mean)',
- 'RES_RES_END-mean': 'Resource resilience (Mean)',
- 'FISH_ECO_END-mean': 'Maintain fish communities (Mean)',
- 'VEG_ECO_END-mean': 'Maintain vegetation communities (Mean)',
- 'INV_ECO_END-mean': 'Maintain invertebrate communities (Mean)',
- 'REC_SPIR_END-mean': 'Maintain recreation and spiritual act (Mean)',
- 'TOURISM_END-mean': 'Maintain tourism (Mean)',
- 'SUB_FISH_END-std': 'Maintaining fisheries for livelihoods (Standard Deviation)',
- 'SUB_VEG_END-std': 'Maintain plants for livelihoods (Standard Deviation)',
- 'LIV_VEG_END-std': 'Maintain plants for domestic livestock (Standard Deviation)',
- 'DOM_WAT_END-std': 'Maintain water for domestic use (Standard Deviation)',
- 'FLO_ATT_END-std': 'Flood attenuation services (Standard Deviation)',
- 'RIV_ASS_END-std': 'River assimilation capacity (Standard Deviation)',
- 'WAT_DIS_END-std': 'Maintain water borne diseases (Standard Deviation)',
- 'RES_RES_END-std': 'Resource resilience (Standard Deviation)',
- 'FISH_ECO_END-std': 'Maintain fish communities (Standard Deviation)',
- 'VEG_ECO_END-std': 'Maintain vegetation communities (Standard Deviation)',
- 'INV_ECO_END-std': 'Maintain invertebrate communities (Standard Deviation)',
- 'REC_SPIR_END-std': 'Maintain recreation and spiritual act (Standard Deviation)',
- 'TOURISM_END-std': 'Maintain tourism (Standard Deviation)'}
-
-
-levels = ['Zero', 'Low', 'Med', 'High']
-input_nodes = ['DISCHARGE_YR', 'DISCHARGE_LF', 'DISCHARGE_HF', 'DISCHARGE_FD', 'WQ_ECOSYSTEM', 'NO_BARRIERS', 'DOM_WAT_GRO', 'WQ_TREATMENT', 'LANDUSE_SSUP', 'WAT_DIS_HUM', 'WQ_PEOPLE', 'WQ_LIVESTOCK']
-output_nodes = ['SUB_VEG_END', 'SUB_FISH_END', 'LIV_VEG_END', 'DOM_WAT_END', 'FLO_ATT_END', 'RIV_ASS_END', 'WAT_DIS_END', 'RES_RES_END', 'FISH_ECO_END', 'VEG_ECO_END', 'INV_ECO_END', 'REC_SPIR_END', 'TOURISM_END']
-
-
-#TODO: these nodes are not set with Zero, Low, Med, High values. Need to have some method for managing their values.
-# probably can extend the netica convenience API to check the type of each node + it's possible input values
-skip_nodes = {
-    # 29 value distribution input from 0-584.7
-    'DISCHARGE_LF', 
-    'DISCHARGE_HF', 
-    'DISCHARGE_YR',     
-    'DISCHARGE_FD',
+# list of nodes to record the state of at the end of the simulation
+# map from the raw name to what the node should be called in the output csv
+output_nodes = {
+    'SUB_VEG_END':  'Maintain plants for livelihoods',
+    'SUB_FISH_END': 'Maintaining fisheries for livelihoods',
+    'LIV_VEG_END':  'Maintain plants for domestic livestock',
+    'DOM_WAT_END':  'Maintain water for domestic use',
+    'FLO_ATT_END':  'Flood attenuation services',
+    'RIV_ASS_END':  'River assimilation capacity',
+    'WAT_DIS_END':  'Maintain water borne diseases',
+    'RES_RES_END':  'Resource resilience',
+    'FISH_ECO_END': 'Maintain fish communities',
+    'VEG_ECO_END':  'Maintain vegetation communities',
+    'INV_ECO_END':  'Maintain invertebrate communities',
+    'REC_SPIR_END': 'Maintain recreation and spiritual act',
+    'TOURISM_END':  'Maintain tourism',
 }
+
+
+# these input nodes need to be retracted before they can be assigned new values
+retract_nodes = {'DISCHARGE_LF', 'DISCHARGE_HF', 'DISCHARGE_YR', 'DISCHARGE_FD'}
 
 
 #functions for getting the mean and standard deviation of the output nodes
 def get_stats(node:int|str|NeticaNode, net:NeticaGraph):
     """Given a histogram of beliefs (Zero, Low, Medium, High), compute mean and std-dev"""
-    beliefs = np.array([net.get_node_belief(node, level) for level in levels]) #np.array([N.GetNodeBelief(out, level, net) for level in Val])
+    #TODO: replace beliefs array with just getting the array of state values via the API
+    #      also need to make the centers use use the correct number of states, and have the correct bounds
+    #      currently it's hardcoded for 4 states (zero, low, medium, high) -> (0, 25, 50, 75, 100)
+    beliefs = np.array([net.get_node_belief(node, i) for i in range(net.get_num_node_states(node))])
     centers = np.arange(4)*25 + 12.5
     mean = np.sum(beliefs*centers)
 
@@ -61,7 +43,6 @@ def get_stats(node:int|str|NeticaNode, net:NeticaGraph):
     std = np.sqrt(np.sum(y*(x-mean)**2)/samples)*2
 
     return mean, std
-
 
 def main():
     netica = NeticaManager()
@@ -74,27 +55,16 @@ def main():
 
     # set input values from the config file
     for key, value in input.items():
-        if key in skip_nodes:
-            print(f"skipping {key} because it has been marked as skip")
-            continue
         if value is not None:
-            if key not in input_nodes:
-                raise ValueError(f"invalid input: {key}:{value}. Key must be one of {input_nodes}")
-            if value not in levels:
-                raise ValueError(f"invalid input: {key}:{value}. Value must be one of {levels}")
-
-            net.enter_finding(key, value, verbose=True)
+            net.enter_finding(key, value, retract=(key in retract_nodes), verbose=True)
 
 
 
     #crate a dataframe with [Catchment,Year,Level,*[*output_nodes x (*levels + ['mean', 'std'])]] as the columns, and Val as the rows
     columns = ['Country', 'Catchment', 'Year']
-    for out in output_nodes:
-        for level in levels:
-            columns.append(f'{out}-{level}')
-        columns.append(f'{out}-mean')
-        columns.append(f'{out}-std')
-    columns_cleaned = [c.replace("b'","").replace("'","") for c in columns]
+    for out in output_nodes.values():
+        columns.append(f'{out} (Mean)')
+        columns.append(f'{out} (Standard Deviation)')
 
     #constant fields for all values
     country = 'South Africa'
@@ -104,22 +74,18 @@ def main():
 
     #output results as a single row for each combination of Out x Val and Out x ['mean', 'std']
     row = [country, catchment, year]
-    for out in output_nodes:
-        for level in levels:
-            # belief = N.GetNodeBelief(out, level, net)
-            belief = net.get_node_belief(out, level)
-            row.append(belief)
-        mean, std = get_stats(out, net)
-        row.append(mean)
-        row.append(std)
+    for raw_name in output_nodes:
+        row.extend(get_stats(raw_name, net))
 
-    results = pd.DataFrame([row], columns=columns_cleaned)
+    # merge the results with the shapefile
+    results = pd.DataFrame([row], columns=columns)
     shape = pd.read_csv('shapes/limpopo_0.1degree.csv')
-    shape = shape[['latitude','longitude','RR']].copy()
-    shape['Country'] = 'South Africa'
-    df = pd.merge(shape, results, left_on = ['Country'], right_on = ['Country'])
-    df = df[df.columns.drop(list(df.filter(regex='Zero|Low|Med|High')))]
-    df.rename(columns=column_mapper, inplace=True)
+    shape['Year'] = year
+    shape['Country'] = country
+    shape['Catchment'] = catchment
+    shape = shape[['Year','latitude','longitude', 'Country', 'Catchment', 'RR']].copy()
+    merge_on = ['Country', 'Year', 'Catchment']
+    df = pd.merge(shape, results, left_on=merge_on, right_on=merge_on)
 
     #save to csv
     print(f'saving to {catchment}.csv')
